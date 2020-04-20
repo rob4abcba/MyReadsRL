eccaaacd372c366c8a003abd313870252e9f89e8
rob4abcba <rob_abcba@yahoo.com>
2020-04-19 20:38:45 -0700

    Rename Header to simply "MyReads"

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return(
             <div className="list-books-title">
-            <h1>MyReads4</h1>
+            <h1>MyReads</h1>
           </div>
         )
     }
