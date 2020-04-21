172f1938ca01a2b83e1a68a2ec81c6d20f9482ea
rob4abcba <rob_abcba@yahoo.com>
2020-04-20 19:25:35 -0700

    Remov comment "Surround <App/> with <BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-import { BrowserRouter } from "react-router-dom";
-// Surround <App /> with <BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(<BrowserRouter>   <App />   </BrowserRouter>, document.getElementById('root'))
